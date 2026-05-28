d403f92f5c3ffae550f250658106a635f673f86e
YongbonJeon <bon6143@gmail.com>
2026-05-28 16:13:47 +0900

    [feature] calculateLengthScore

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,12 +1,23 @@
 public class SimilarityChecker {
-    public int calculateLengthScore(String alphabets) {
-        assertIllegalArgument(alphabets);
+    public double calculateLengthScore(String alphabetsA, String alphabetsB) {
+        assertIllegalArgument(alphabetsA, alphabetsB);
 
-        return 0;
+        if(alphabetsA.length() * 2 <= alphabetsB.length()
+        || alphabetsB.length() * 2 <= alphabetsA.length()) {
+            return 0;
+        }
+        int diffLength = Math.abs(alphabetsA.length() - alphabetsB.length());
+
+        return 60 - diffLength * 60 / Math.min(alphabetsA.length(), alphabetsB.length());
     }
 
-    private static void assertIllegalArgument(String alphabets) {
-        for (char alphabet : alphabets.toCharArray()) {
+    private static void assertIllegalArgument(String alphabetsA, String alphabetsB) {
+        for (char alphabet : alphabetsA.toCharArray()) {
+            if(alphabet < 'A' || alphabet > 'Z') {
+                throw new IllegalArgumentException();
+            }
+        }
+        for (char alphabet : alphabetsB.toCharArray()) {
             if(alphabet < 'A' || alphabet > 'Z') {
                 throw new IllegalArgumentException();
             }
--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -22,17 +22,43 @@ class SimilarityCheckerTest {
 
     @Test
     void assertInvalidInput() {
-        assertIllegalArgument("123");
-        assertIllegalArgument("aAB");
-
-
+        assertIllegalArgument("123", "AAB");
+        assertIllegalArgument("aAB", "AAA");
     }
 
-    private void assertIllegalArgument(String alphabets) {
+    private void assertIllegalArgument(String alphabetsA, String alphabetsB) {
         assertThrows(
                 IllegalArgumentException.class,
-                () -> similarityChecker.calculateLengthScore(alphabets)
+                () -> similarityChecker.calculateLengthScore(alphabetsA, alphabetsB)
         );
     }
 
+
+    @Test
+    void calculateLengthScoreTest_ASD_DSA() {
+        double lengthScore = similarityChecker.calculateLengthScore("ASD", "DSA");
+
+        assertThat(lengthScore).isEqualTo(60);
+    }
+
+    @Test
+    void calculateLengthScoreTest_A_BB() {
+        double lengthScore = similarityChecker.calculateLengthScore("A", "BB");
+
+        assertThat(lengthScore).isEqualTo(0);
+    }
+
+    @Test
+    void calculateLengthScoreTest_AAABB_BAA() {
+        double lengthScore = similarityChecker.calculateLengthScore("AAABB", "BAA");
+
+        assertThat(lengthScore).isEqualTo(20);
+    }
+
+    @Test
+    void calculateLengthScoreTest_AA_AAE() {
+        double lengthScore = similarityChecker.calculateLengthScore("AA", "AAE");
+
+        assertThat(lengthScore).isEqualTo(30);
+    }
 }
