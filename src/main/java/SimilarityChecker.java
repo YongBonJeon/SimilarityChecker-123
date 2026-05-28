e73e2a9cb12fee8f107d305bf080eeaaf8ba9fc3
YongbonJeon <bon6143@gmail.com>
2026-05-28 15:57:26 +0900

    [feature] inValidInput

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,2 +1,12 @@
 public class SimilarityChecker {
+    public int calculateLengthScore(String alphabets) {
+
+        for (char alphabet : alphabets.toCharArray()) {
+            if(alphabet < 'A' || alphabet > 'Z') {
+                throw new IllegalArgumentException();
+
+            }
+        }
+        return 0;
+    }
 }
--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -1,10 +1,37 @@
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityCheckerTest {
 
+    private SimilarityChecker similarityChecker;
 
+    @BeforeEach
+    void setUp() {
+        similarityChecker = new SimilarityChecker();
+    }
+
+    @Test
+    void createSimilarityChecker() {
+
+        assertThat(similarityChecker).isNotNull();
+    }
+
+    @Test
+    void assertInvalidInput() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> similarityChecker.calculateLengthScore("123")
+        );
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> similarityChecker.calculateLengthScore("aAB")
+        );
+
+
+    }
 
 }
