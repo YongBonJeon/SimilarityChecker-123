24e811ed2a9660084d8a9d3e7394b2a48ea04b95
YongbonJeon <bon6143@gmail.com>
2026-05-28 16:18:09 +0900

    [refactoring] calculateLengthScore

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -2,16 +2,29 @@ public class SimilarityChecker {
     public double calculateLengthScore(String alphabetsA, String alphabetsB) {
         assertIllegalArgument(alphabetsA, alphabetsB);
 
-        if(alphabetsA.length() * 2 <= alphabetsB.length()
-        || alphabetsB.length() * 2 <= alphabetsA.length()) {
+        if(isLengthDiffTwiceOrMore(alphabetsA, alphabetsB)) {
             return 0;
         }
-        int diffLength = Math.abs(alphabetsA.length() - alphabetsB.length());
 
-        return 60 - diffLength * 60 / Math.min(alphabetsA.length(), alphabetsB.length());
+        int lengthDiff = calculateLengthDiff(alphabetsA, alphabetsB);
+
+        return calculatePartialScore(alphabetsA, alphabetsB, lengthDiff);
     }
 
-    private static void assertIllegalArgument(String alphabetsA, String alphabetsB) {
+    private int calculatePartialScore(String alphabetsA, String alphabetsB, int lengthDiff) {
+        return 60 - lengthDiff * 60 / Math.min(alphabetsA.length(), alphabetsB.length());
+    }
+
+    private static int calculateLengthDiff(String alphabetsA, String alphabetsB) {
+        return Math.abs(alphabetsA.length() - alphabetsB.length());
+    }
+
+    private boolean isLengthDiffTwiceOrMore(String alphabetsA, String alphabetsB) {
+        return alphabetsA.length() * 2 <= alphabetsB.length()
+                || alphabetsB.length() * 2 <= alphabetsA.length();
+    }
+
+    private void assertIllegalArgument(String alphabetsA, String alphabetsB) {
         for (char alphabet : alphabetsA.toCharArray()) {
             if(alphabet < 'A' || alphabet > 'Z') {
                 throw new IllegalArgumentException();
